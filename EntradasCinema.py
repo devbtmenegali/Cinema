1921aeb46319cddf78a0e1a348e94b6139798c98
devbtmenegali <brunotmenegali@gmail.com>
2024-09-08 20:59:45 -0300

    Fun Escolher Assento

--- EntradasCinema.py
+++ EntradasCinema.py
@@ -1,6 +1,42 @@
 import random
+import numpy as np
 
-def filmes_em_cartaz ():
+
+def funcao_filmes_em_cartaz (dicionario_filmes):
+
+    for codigo, titulo in dicionario_filmes.items():
+        print(f"{codigo} - {titulo}")
+
+
+def funcao_escolher_filmes(dicionario_filmes):
+
+    escolher_filme = int(input("Digite o código do filme:"))
+
+    if escolher_filme in dicionario_filmes.keys():
+        filme_escolhido = dicionario_filmes[escolher_filme]
+        print (f"Você escolheu o filme {filme_escolhido}")
+        funcao_escolher_assento()
+
+    else:
+        print("O código digitado está errado!")
+
+    return escolher_filme
+
+def funcao_escolher_assento():
+
+    decisao_do_assento = str(input("VOcê gostaria de escolher o assento? S/N ")).upper()
+
+    if decisao_do_assento == "N":
+        numero_do_assento = random.randint(1, 20)
+        print(f"Seu número do assento é: {numero_do_assento}")
+
+    elif decisao_do_assento == "S":
+        numeros = np.arange(1, 21)
+        matriz = numeros.reshape(4, 5)
+        print(matriz)
+            
+
+def main():
 
     print ("QUAL FILME VOCÊ QUER ASSISTIR?")
     
@@ -9,22 +45,11 @@ def filmes_em_cartaz ():
                         3: "Piratas do Caribe",
                         4: "Avatar",
                         5: "Mad Max"}
-
-    for codigo, titulo in dicionario_filmes.items():
-        print(f"{codigo} - {titulo}")
-
-
-def escolher_filmes():
-
     
+    funcao_filmes_em_cartaz(dicionario_filmes)  
+    escolher_filme = funcao_escolher_filmes(dicionario_filmes)
 
-def main():
-
-    filmes_em_cartaz()
-
-
-
-
+    funcao_escolher_assento()
 
 if __name__=="__main__":
 
