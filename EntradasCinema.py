33e744e6a8a85239f08b8aeaa5aa0a6b36b22bf8
devbtmenegali <brunotmenegali@gmail.com>
2024-09-08 20:13:38 -0300

    Filmes em Cartaz

---
+++ EntradasCinema.py
@@ -0,0 +1,31 @@
+import random
+
+def filmes_em_cartaz ():
+
+    print ("QUAL FILME VOCÊ QUER ASSISTIR?")
+    
+    dicionario_filmes = {1: "Titanic", 
+                        2: "Senhor dos Aneis",
+                        3: "Piratas do Caribe",
+                        4: "Avatar",
+                        5: "Mad Max"}
+
+    for codigo, titulo in dicionario_filmes.items():
+        print(f"{codigo} - {titulo}")
+
+
+def escolher_filmes():
+
+    
+
+def main():
+
+    filmes_em_cartaz()
+
+
+
+
+
+if __name__=="__main__":
+
+    main()
