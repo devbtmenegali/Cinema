0ed565da4334db2feb462f4e65a8807409114a3c
devbtmenegali <brunotmenegali@gmail.com>
2024-09-08 22:29:07 -0300

    Versão FInal

--- EntradasCinema.py
+++ EntradasCinema.py
@@ -6,7 +6,9 @@ def funcao_filmes_em_cartaz(dicionario_filmes):
         print(f"{codigo} - {titulo}")
 
 def funcao_escolher_filmes(dicionario_filmes):
+    
     escolher_filme = int(input("Digite o código do filme: "))
+   
     if escolher_filme in dicionario_filmes:
         filme_escolhido = dicionario_filmes[escolher_filme]
         print(f"Você escolheu o filme {filme_escolhido}")
@@ -40,6 +42,24 @@ def escolher_assento(matriz):
         print("Assento inválido!")
         return None
 
+def formas_de_pagamentos(dicionario_formas_de_pagamento):
+
+    for codigo, pagamento in dicionario_formas_de_pagamento.items():
+        print(f"{codigo} - {pagamento}")
+
+def escolher_forma_de_pagamento(dicionario_formas_de_pagamento):
+
+    forma_de_pagamento = int(input("Digite o código referente a forma de pagamento: "))
+
+    if forma_de_pagamento in dicionario_formas_de_pagamento:
+        opcao_pagamento = dicionario_formas_de_pagamento[forma_de_pagamento]
+        return opcao_pagamento
+
+    else:
+        print("Código de pagamento inválido!")
+        return None
+
+
 def main():
     print("QUAL FILME VOCÊ QUER ASSISTIR?")
     
@@ -62,6 +82,25 @@ def main():
             print(f"Filme: {filme_escolhido}")
             print(f"Assento: {assento_escolhido}")
             print("Valor.... R$ 30,00")
+            print("\nQUAL SERÁ A FORMA DE PAGAMENTO?")
+    
+    dicionario_formas_de_pagamento = {
+        1: "PIX",
+        2: "Cartão",
+        3: "Dinheiro"
+    }
+    formas_de_pagamentos(dicionario_formas_de_pagamento)
+    opcao_pagamento = escolher_forma_de_pagamento(dicionario_formas_de_pagamento)
 
+    if opcao_pagamento:
+        print("\nRECIBO DO PEDIDO")
+        print(f"Filme: {filme_escolhido}")
+        print(f"Assento: {assento_escolhido}")
+        print("Valor.... R$ 30,00")
+        print(f"Pagamento: {opcao_pagamento}")
+        print(f"TENHA UM EXCELENTE FILME!")
+    else:
+        print("Forma de pagamento inválida.")
+        
 if __name__ == "__main__":
     main()
